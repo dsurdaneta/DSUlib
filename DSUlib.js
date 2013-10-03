22e525de711b6fc51d71f7223c84731e1309b93a
Daniel Urdaneta <mangafever@gmail.com>
2013-10-03 09:29:58 -0430

    first commit of the library
    3 functions (mensaje, toggle_visibility and toggle_hover_visibility)

---
+++ DSUlib.js
@@ -0,0 +1,50 @@
+/**mensaje.js**/
+var mensaje= function(msg, typeM){
+    if(typeM==='alert'){
+        alert(msg);
+    }
+    if(typeM==='prompt'){
+        prompt(msg);
+    }
+    if(typeM==='confirm'){
+        confirm(msg);
+    }
+};
+
+//mensaje('Hola','prompt');
+
+/**toggle visibilty
+shows/hides visibility of an element, via click or hover.
+It chages an element disblay status between 'block' and 'none'
+**/
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+	
+	function toggle_hover_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+	
+/*
+Ex:
+<button type="button" title="Show/Hide" onclick="toggle_visibility('menu_categories')"> Categories </button>
+
+<div id="menu_categories" style="display:block;">
+	...Content...
+</div>
+	
+<h1><strong><a onmouseover="toggle_hover_visibility('subcategories')"  href="...source...">Text</a> </strong></h1>
+<ul id="subcategories" style="display:none;">
+	<li></li>
+</ul>
+*/
+
