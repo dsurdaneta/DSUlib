0b73cab6fbfdf684bdf314fcc0b2cd60e330985d
Daniel Urdaneta <mangafever@gmail.com>
2013-10-31 16:10:08 -0430

    Sendmail and minor js scripts
    send mail (php), menubar js and loading a page from a select with js

--- DSUlib.js
+++ DSUlib.js
@@ -63,3 +63,18 @@ function color_timer(){
 		    }
 		} 
 }
+
+/***menubar***/
+$(document).ready(function() {
+   // Muestra y oculta los menús
+   $('ul li:has(ul)').hover(
+      function(e)
+      {
+         $(this).find('ul').css({display: "block"});
+      },
+      function(e)
+      {
+         $(this).find('ul').css({display: "none"});
+      }
+   );
+});
